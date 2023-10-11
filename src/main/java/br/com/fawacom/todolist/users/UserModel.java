607dd265a718ead6e8957a261b83860b86ca8234
wr2net <wagnerr2@gmail.com>
2023-10-10 22:26:03 -0300

    feat: Aula 02 - Integração com Banco de Dados.

--- src/main/java/br/com/fawacom/todolist/users/UserModel.java
+++ src/main/java/br/com/fawacom/todolist/users/UserModel.java
@@ -1,8 +1,29 @@
 package br.com.fawacom.todolist.users;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+@Data
+@Entity(name = "tb_users")
 public class UserModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
+    @Column(unique = true)
     private String username;
     private String name;
     private String password;
     private String email;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
