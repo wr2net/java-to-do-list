a8ecfab479870c107f4d41cc4e3d89db27e5fcd6
wr2net <wagnerr2@gmail.com>
2023-10-13 10:54:25 -0300

    feat: Aula 05 - Atualizando tarefas e validação de rotas.

--- src/main/java/br/com/fawacom/todolist/users/UserController.java
+++ src/main/java/br/com/fawacom/todolist/users/UserController.java
@@ -1,13 +1,13 @@
 package br.com.fawacom.todolist.users;
 
 import at.favre.lib.crypto.bcrypt.BCrypt;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
@@ -15,6 +15,7 @@ public class UserController {
 
     @Autowired
     private IUserRepository userRepository;
+
     @PostMapping("/new")
     public ResponseEntity create(@RequestBody UserModel userModel) {
         var user = this.userRepository.findByUsername(userModel.getUsername());
@@ -28,5 +29,10 @@ public class UserController {
         userModel.setPassword(passwordHashred);
         var userCreated = this.userRepository.save(userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
+    }
+
+    @GetMapping("/")
+    public List<UserModel> list() {
+        return this.userRepository.findAll();
     }
 }
